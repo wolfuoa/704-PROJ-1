39a923dfe79faf35a8dcdb3fa651ddc3477b1ed8
Nicholas Wolf <100742628+wolfuoa@users.noreply.github.com>
2024-09-02 11:58:23 +1200

    refactor: :fire: renaming fluids numbers

--- src/abs/src/util/Order.java
+++ src/abs/src/util/Order.java
@@ -4,18 +4,18 @@ import java.io.Serializable;
 
 public class Order implements Serializable{
     private final int quantity;
+    private final int fluid_0;
     private final int fluid_1;
     private final int fluid_2;
     private final int fluid_3;
-    private final int fluid_4;
 
 
-    public Order(int quantity, int fluid_1, int fluid_2, int fluid_3, int fluid_4) {
+    public Order(int quantity, int fluid_0, int fluid_1, int fluid_2, int fluid_3) {
         this.quantity = quantity;
+        this.fluid_0 = fluid_0;
         this.fluid_1 = fluid_1;
         this.fluid_2 = fluid_2;
         this.fluid_3 = fluid_3;
-        this.fluid_4 = fluid_4;
     }
 
     public int getQuantity() {
@@ -25,14 +25,14 @@ public class Order implements Serializable{
     public int getFluid(int fluid_num) {
     	switch(fluid_num)
     	{
+    	case 0:
+    		return this.fluid_0;
     	case 1:
     		return this.fluid_1;
     	case 2:
     		return this.fluid_2;
     	case 3:
     		return this.fluid_3;
-    	case 4:
-    		return this.fluid_4;
     	default:
     		break;
     	}
