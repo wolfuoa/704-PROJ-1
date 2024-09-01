5a02b89ab56e6bce24b218d1f867ebc3e8814546
wolfuoa <nwol626@aucklanduni.ac.nz>
2024-09-01 15:11:51 +1200

    feat: :sparkles: Serializable Order object

---
+++ src/abs/src/util/json.java
@@ -0,0 +1,27 @@
+import java.io.Serializable;
+
+public class Order implements Serializable{
+    private final int quantity;
+    private final int fluid_1;
+    private final int fluid_2;
+    private final int fluid_3;
+    private final int fluid_4;
+
+
+    public Order(int quantity, int fluid_1, int fluid_2, int fluid_3, int fluid_4) {
+        this.quantity = quantity;
+        this.fluid_1 = fluid_1;
+        this.fluid_2 = fluid_2;
+        this.fluid_3 = fluid_3;
+        this.fluid_4 = fluid_4;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public int[] getFluids() {
+        int[] ret = {this.fluid_1, this.fluid_2, this.fluid_3, this.fluid_4};
+        return ret;
+    }
+}
