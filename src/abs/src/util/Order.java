9c8f99c96fc3dac5cc86033ae147ff45f1bd58c1
Nicholas Wolf <100742628+wolfuoa@users.noreply.github.com>
2024-09-02 13:30:19 +1200

    refactor: :white_check_mark: Rename, testing, fully functional liquid filler

--- src/abs/src/util/Order.java
+++ src/abs/src/util/Order.java
@@ -4,35 +4,35 @@ import java.io.Serializable;
 
 public class Order implements Serializable{
     private final int quantity;
-    private final int fluid_0;
-    private final int fluid_1;
-    private final int fluid_2;
-    private final int fluid_3;
+    private final int volume_liquid_0;
+    private final int volume_liquid_1;
+    private final int volume_liquid_2;
+    private final int volume_liquid_3;
 
 
-    public Order(int quantity, int fluid_0, int fluid_1, int fluid_2, int fluid_3) {
+    public Order(int quantity, int volume_liquid_0, int volume_liquid_1, int volume_liquid_2, int volume_liquid_3) {
         this.quantity = quantity;
-        this.fluid_0 = fluid_0;
-        this.fluid_1 = fluid_1;
-        this.fluid_2 = fluid_2;
-        this.fluid_3 = fluid_3;
+        this.volume_liquid_0 = volume_liquid_0;
+        this.volume_liquid_1 = volume_liquid_1;
+        this.volume_liquid_2 = volume_liquid_2;
+        this.volume_liquid_3 = volume_liquid_3;
     }
 
     public int getQuantity() {
         return this.quantity;
     }
 
-    public int getFluid(int fluid_num) {
-    	switch(fluid_num)
+    public int getLiquidVolume(int liquid_num) {
+    	switch(liquid_num)
     	{
     	case 0:
-    		return this.fluid_0;
+    		return this.volume_liquid_0;
     	case 1:
-    		return this.fluid_1;
+    		return this.volume_liquid_1;
     	case 2:
-    		return this.fluid_2;
+    		return this.volume_liquid_2;
     	case 3:
-    		return this.fluid_3;
+    		return this.volume_liquid_3;
     	default:
     		break;
     	}
