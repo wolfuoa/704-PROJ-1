34d7140e94cb18d999e637ba2940ad1b90ceed9d
Nicholas Wolf <100742628+wolfuoa@users.noreply.github.com>
2024-09-03 14:49:12 +1200

    feat: :sparkles: New positions, baxter right arm

---
+++ src/abs/src/util/Bottle.java
@@ -0,0 +1,12 @@
+public class Bottle {
+    private Status[] bottleStatuses;
+    
+    private enum Status {
+        FILLED,
+        CAPPED;
+    }
+
+    add status
+    
+    get statuses
+}
--- src/abs/src/util/RoboticArm.java
+++ src/abs/src/util/RoboticArm.java
@@ -1,18 +1,32 @@
 package util;
 
-public class RoboticArm {
+import java.io.Serializable;
+
+public class RoboticArm implement Serializable {
     private ArmStatus armStatus;
     private GripperStatus gripperStatus;
 
     public RoboticArm() {
-        this.armStatus = ArmStatus.POSITION_A;
+        this.armStatus = ArmStatus.POSITION_IDLE;
         this.gripperStatus = gripperStatus.OPEN;
     }
 
     public enum ArmStatus {
-        POSITION_A,
-        POSITION_B,
-        POSITION_C;
+        POSITION_IDLE,
+        POSITION_ABOVE_COLLECTION_POINT,
+        POSITION_ON_COLLECTION_POINT,
+        POSITION_ABOVE_CONVEYOR_START,
+        POSITION_ON_CONVEYOR_START,
+        POSITION_ABOVE_REMOVAL_POINT,
+        POSITION_ON_REMOVAL_POINT,
+        POSITION_ABOVE_PACKAGE,
+        POSITION_ON_PACKAGE,
+        POSITION_ABOVE_LID_SURPLUS,
+        POSITION_ON_LID_SURPLUS,
+        POSITION_ABOVE_POSITION_THREE,
+        POSITION_ON_POSITION_THREE,
+        POSITION_ABOVE_ROTARY_TURNTABLE,
+        POSITION_ON_ROTARY_TURNTABLE;
     }
 
     public enum GripperStatus {
